329326ecb06b0b39d74c5d0805d52d1dedd38bb1
lc9900 <whynot_hm@hotmail.com>
2017-07-09 14:59:47 -0400

    initialize bash.js

---
+++ test.js
@@ -0,0 +1,7 @@
+// This is how nodeJs import a library
+var http = require('http');
+var server = http.createServer(function(req, resp){
+  // This is the callback function
+});
+server.listen(3000);
+
